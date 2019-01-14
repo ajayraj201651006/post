dbe6f07e3a30f80f1d4d4adc27d25149714e3d86
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-14 23:53:26 +0530

    update

---
+++ src/components/NewPost/NewPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './NewPost.css';
+
+const newPost = (props) => {
+	return(
+           <div className={classes.NewPost}>
+                <h1>Add a Post</h1>
+           </div>
+		);
+}
+
+export default newPost;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './Blog.css';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
 
 class Blog extends Component {
 	render() {
@@ -14,6 +15,9 @@ class Blog extends Component {
                   </section>
                   <section>
                     <FullPost />
+                  </section>
+                  <section>
+                    <NewPost />
                   </section>
                </div>
 			)
