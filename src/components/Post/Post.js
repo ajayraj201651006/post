b6b8ed9980a22cc36da7cd8664526e1801e43316
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-14 20:30:31 +0530

    update

---
+++ src/components/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Post.css';
+
+const post = (props) => {
+        return(
+               <div className={classes.Post}>
+                  <h3>Title</h3>
+               </div>
+        	);
+}
+
+export default post;
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import classes from './Blog.css';
+import Post from '../../components/Post/Post';
 
 class Blog extends Component {
 	render() {
 		return(
                <div>
-                  <h1>Hello React</h1>
+                  <Post />
                </div>
 			)
 	}
