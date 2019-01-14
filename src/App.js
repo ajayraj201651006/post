10864febae2d6ca3ceea0ba4012f82c71f065377
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-14 21:45:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Blog from './containers/Blog/Blog';
-
+import Aux from './hoc/Auxes';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Blog />
-      </div>
+        <Aux>
+           <Blog />
+        </Aux>
     );
   }
 }
