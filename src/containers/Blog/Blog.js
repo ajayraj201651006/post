66304bbd2b1c48171fdd20cb4503c51fcb42ab0c
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-14 20:24:05 +0530

    update

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './Blog.css';
+
+class Blog extends Component {
+	render() {
+		return(
+               <div>
+                  <h1>Hello React</h1>
+               </div>
+			)
+	}
+}
+
+export default Blog;
