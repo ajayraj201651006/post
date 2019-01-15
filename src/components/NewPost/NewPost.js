59f26d6daad73722174979f36e1cb449797616b6
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-15 14:24:27 +0530

    update

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,31 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './NewPost.css';
 
-const newPost = (props) => {
-	return(
-           <div className={classes.NewPost}>
-                <h1>Add a Post</h1>
-                <div>
-                    <h4>Title</h4>
+class NewPost extends Component {
+	state = {
+		title: '',
+		content: '',
+		author: 'Max'
+	}
+     render() {
+     	return(
+                <div className={classes.NewPost}>
+                    <h1>Add a Post</h1>
+                    <label>Title</label>
                     <input />
-                </div>
-                <div>
-                    <h4>content</h4>
-                    <input />
-                </div>
-                <div>
-                    <h4>Author</h4>
-                    <select name="cars">
-                        <option value="volvo">Volvo</option>
+	                <label>Content</label>
+	                <textarea />
+	                <label>Author</label>
+	                <select>
+                        <option value="Max">Max</option>
+                        <option value="Manu">Manu</option>
                     </select>
-                    <div>
-	                    <button className={classes.Edit}>
-	                        Add Post
-	                    </button>
-                    </div>
+                    <button>Add Post</button>
                 </div>
-           </div>
-		);
-}
-
-export default newPost;
+     		);
+     }
+} 
+export default NewPost;
