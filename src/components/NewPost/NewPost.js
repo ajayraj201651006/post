254e2dfd58708e92aa92b33ea0fd1473daa68998
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-15 10:15:42 +0530

    update

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -5,6 +5,25 @@ const newPost = (props) => {
 	return(
            <div className={classes.NewPost}>
                 <h1>Add a Post</h1>
+                <div>
+                    <h4>Title</h4>
+                    <input />
+                </div>
+                <div>
+                    <h4>content</h4>
+                    <input />
+                </div>
+                <div>
+                    <h4>Author</h4>
+                    <select name="cars">
+                        <option value="volvo">Volvo</option>
+                    </select>
+                    <div>
+	                    <button>
+	                        Add Post
+	                    </button>
+                    </div>
+                </div>
            </div>
 		);
 }
