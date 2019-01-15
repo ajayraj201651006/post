024cdb211ad3ed5604569bd71718b55ddbfac5d7
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-15 20:48:48 +0530

    update

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,7 +6,7 @@ const post = (props) => {
                 <article className={classes.Post}>
                   <h3>{props.title}</h3>
                   <div className={classes.Info}>
-                    <div className={classes.Author}>Author</div>
+                    <div className={classes.Author}>{props.author}</div>
                   </div>
                 </article>
         	);
