121d698c14710b36a3d31d7c8266c3ef8f01eb93
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-15 22:40:53 +0530

    update

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,7 +3,7 @@ import classes from './Post.css';
 
 const post = (props) => {
         return(
-                <article className={classes.Post}>
+                <article className={classes.Post} onclick={props.clicked}>
                   <h3>{props.title}</h3>
                   <div className={classes.Info}>
                     <div className={classes.Author}>{props.author}</div>
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,7 +7,8 @@ import axios from 'axios';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   }
 
   componentDidMount() {
@@ -23,9 +24,16 @@ class Blog extends Component {
       });
   }
 
+  postSelectedHandler = (id) => {
+      this.setState({selectedPostId: id})
+  }
+
 	render() {
     const posts = this.state.posts.map(post=>{
-       return <Post key={post.id} title={post.title} author={post.author}/>
+       return <Post key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>;
     });
 
 		return(
@@ -34,7 +42,7 @@ class Blog extends Component {
                     {posts}
                   </section>
                   <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                   </section>
                   <section>
                     <NewPost />
