15dc3ca7afe07b5af989ee45753f3592e94715ae
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-23 15:10:02 +0530

    update

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Blog.css';
 import Posts from './Posts/Posts';
+import { Route } from 'react-router-dom';
 
 class Blog extends Component {
 	render() {
@@ -14,7 +15,7 @@ class Blog extends Component {
                       </ul>
                     </nav>
                   </header>
-                  <Posts />
+                  <Route path="/" exact component={Posts}/>
                </div>
 			)
 	}
