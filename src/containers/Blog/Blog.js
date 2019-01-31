a601eb2dd234b0cf4babb566f287859200d4bcc7
ajayraj201651006 <ajayraj003016@gmail.com>
2019-01-31 11:04:09 +0530

    add switch router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import classes from './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
@@ -33,8 +33,10 @@ class Blog extends Component {
                     </nav>
                   </header>
                   <Route path="/" exact component={Posts}/>
-                  <Route path="/new-post" component={NewPost} />
-                  <Route path="/:id" exact component={FullPost} />
+                  <Switch>
+                      <Route path="/new-post" component={NewPost} />
+                      <Route path="/:id" exact component={FullPost} />
+                  </Switch>
                </div>
 			)
 	}
