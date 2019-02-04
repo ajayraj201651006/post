82aca59cd3c7305ea2c9fa87093f56624ab4e4f3
ajayraj201651006 <ajayraj003016@gmail.com>
2019-02-04 18:55:57 +0530

    load lazy

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import classes from './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 // import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+     return import('./NewPost/NewPost');
+})
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
 
 	render() {
@@ -37,7 +42,7 @@ class Blog extends Component {
                     </nav>
                   </header>
                   <Switch>
-                      {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                      {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                       <Route path="/posts" component={Posts}/>
                       <Route render={() => <h1>Not Found</h1>} />
                       {/*<Redirect from="/" to="/posts" />*/}
